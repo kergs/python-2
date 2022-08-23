bd1571b726c39eca3ce56eef61d1ab3dde3e5de2
kergs <cchisomfrancis@gmail.com>
2022-08-23 13:57:44 +0100

    youtube

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -3,11 +3,11 @@ import pytube
 print("Enter Video url")
 url = input(":>> ")
 
-#print("Enter path for storage")
-#path = input(":>> ")
+print("Enter path for storage")
+path = input(":>> ")
 
 #specifying the storage path of the video, either path can be used
-path = "E:"
+#path = "E:"
 
 #main code line to download the video
-pytube.Youtube(url).streams.get_highest_resolution().download(path)
+pytube.YouTube(url).streams.get_highest_resolution().download(path)
